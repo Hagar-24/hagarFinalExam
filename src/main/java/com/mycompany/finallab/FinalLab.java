917c67d1b3bf17b9d31de454573d4f2ae18b2e24
Hagar-24 <hagarnaga24@gmail.com>
2025-02-05 18:14:11 +0300

    Initial commit

---
+++ src/main/java/com/mycompany/finallab/FinalLab.java
@@ -0,0 +1,13 @@
+
+package com.mycompany.finallab;
+
+/**
+ *
+ * @author hagar
+ */
+public class FinalLab {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
